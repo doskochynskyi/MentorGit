b1132ed66be0fee697f1368d5e15819a84ffb682
Ivan <ivan.doskochynskyi@gmail.com>
2022-08-01 08:35:46 +0300

    update script

--- scripts/devfile.sh
+++ scripts/devfile.sh
@@ -1,1 +1,15 @@
 echo "Text from dev script file"
+
+git config user.name Ivan
+git config user.email ivan.doskochynskyi@gmail.com
+#git commit -am "Config file copied from MentorTF module"
+#git push
+
+if [ ! -d "./aksrepo" ] 
+then
+    echo "Directory ./aksrepo does not exists." 
+    mkdir ./aksrepo
+    ls ./
+else
+    echo "Directory exists."
+fi
